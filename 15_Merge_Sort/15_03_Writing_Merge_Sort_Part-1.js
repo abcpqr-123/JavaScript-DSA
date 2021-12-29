c690796a0c04fac2a790beab5518421e916b1858
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2021-12-29 10:44:47 +0530

    Add files via upload

---
+++ 15_Merge_Sort/15_03_Writing_Merge_Sort_Part-1.js
@@ -0,0 +1,12 @@
+// mergeSort Pseudocode:
+
+
+// * Break up the array into halves until u have arrays that are 
+//   empty or have one element 
+// * Once u have smaller sorted arrays, merge those arrays with  
+//   other sorted arrays until u are back at the full length of the 
+//   array 
+// * Once the array has been merged back together, return the merged 
+//   (and sorted!) arrays
+
+
