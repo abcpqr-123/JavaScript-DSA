fd64d9a4e0f7bd2b459a1ca3118042b92ad8efcd
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2021-12-29 10:42:51 +0530

    Add files via upload

---
+++ 14_Comparision_Bubble_Selection_Insertion/14_01_Comparision_Bubble_Selection_Insertion.js
@@ -0,0 +1,15 @@
+// Big O   SORTING AlGORITHMS 
+
+
+// algorithm    Time Complexity    TimeComplexity    TimeComplexity     Space Complexity 
+//                   (Best)          (Average)          (Worst)             
+// Bubble Sort        O(n)            O(n)^2             O(n)^2              O(1)
+// Insertion Sort     O(n)            O(n)^2             O(n)^2              O(1)
+// Selection SOrt     O(n)^2          O(n)^2             O(n)^2              O(1)
+
+
+
+// Insertion and bubble are the good if the array is sorted. 
+// If not then they are also not good
+
+// Seleddction sort is very slow.
