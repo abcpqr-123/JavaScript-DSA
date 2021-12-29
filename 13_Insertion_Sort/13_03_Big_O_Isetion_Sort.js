a839a2d7da8b80e25ba7987b110ee7d5940259e1
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2021-12-29 10:42:23 +0530

    Add files via upload

---
+++ 13_Insertion_Sort/13_03_Big_O_Isetion_Sort.js
@@ -0,0 +1,14 @@
+// algorithm    Time Complexity    TimeComplexity    TimeComplexity     Space Complexity 
+//                   (Best)          (Average)          (Worst)             
+// Bubble Sort        O(n)            O(n)^2             O(n)^2              O(1)
+// Insertion Sort     O(n)            O(n)^2             O(n)^2              O(1)
+// Selection SOrt     O(n)^2          O(n)^2             O(n)^2              O(1)
+
+
+// Insertion srot: worst case - Object(n)^2
+
+
+// but if the data is almost sorted then it will be the best case.
+
+// so, In a situation where sata is coming in live or streamed and
+// u need to insert it at moment's notice. 
