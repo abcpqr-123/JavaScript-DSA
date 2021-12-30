6edf571ceb1fb499a0ff05fee4526c6bcb48db08
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2021-12-30 14:02:19 +0530

    Add files via upload

---
+++ 18_Data_Structures_Intro/18_01_Data_Structures_Intro.js
@@ -0,0 +1,15 @@
+// What do data structures do? 
+
+// Data structures are collections of values, the relationships among them,
+// and the funcions of oprations that can be applied to the data
+
+
+// working with map or location data - u can use Graph
+
+// If u Need an ordered list with fast inserts/removals at the begging 
+// and end  - u can use LinkedList
+
+// If u r web screaping nested HTML - u can use Tree 
+
+// If u need to write a scheduler - u can use Heap
+
