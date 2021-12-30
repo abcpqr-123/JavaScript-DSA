8d1303ef0787a11845e16018f8ace56d6b051b4e
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2021-12-30 14:05:33 +0530

    Add files via upload

---
+++ 18_Data_Structures_Intro/18_05_02_How_Using_Classes.js
@@ -0,0 +1,24 @@
+// How we will be using classes 
+
+// class DataStructure(){
+//     constructor(){
+//         // what default properties should it have 
+//     }
+//     someInstanceMethod(){
+//         // what should each object created from this class be able to do?
+//     }
+// }
+
+
+// we will be using the 'constructor' and 'instance mentods' quite a bit!
+
+// we will almost never be using 'satatic' methods
+
+
+
+
+
+// One gotcha with 'this'
+
+// Inside all of our 'instance' methods and 'constructor', the keyword 'this'
+// refers to the object created form the class (also known s an 'instance')
