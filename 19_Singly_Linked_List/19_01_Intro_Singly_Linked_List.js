9ece65c4f33857459ac5001999724617ce15f790
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-01 21:21:32 +0530

    Add files via upload

---
+++ 19_Singly_Linked_List/19_01_Intro_Singly_Linked_List.js
@@ -0,0 +1,26 @@
+// What is a linked list? 
+
+// A data structure that contains a 'Head', 'tail', and 'length', property 
+
+
+// Linked Lists consist of nodes, and each 'node' has a 'value' and a 
+// 'pointer' to another node or null 
+
+
+// Comparision with Arrays: 
+
+// Lists:
+// * Do not have indexes!
+
+// * Connected via nodes witha a next pointer 
+
+// * Random access is not allowed
+
+
+
+// Arrays:
+// * Indexed in Order! 
+
+// * Insertion and deletion can be expensive 
+
+// * Can quickly be accessed at a specific index
