0dd1240b3d9f7c4a8c7fc901c54daed1bbc88b8c
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-02 18:08:47 +0530

    Add files via upload

---
+++ 19_Singly_Linked_List/19_10_Get_Intro.js
@@ -0,0 +1,16 @@
+// Get
+
+// Retrieving a node by its position in the linked list 
+// (getting a node by its position in the linked list)
+
+
+// Get pseudocode 
+
+// * This function should accept an indexe
+
+// * If the index is less than zero or greater than or equeal to the length of 
+//   the list, return null 
+
+// * Loop through the list until u reach the index and return the node at that specific 
+//   index 
+
