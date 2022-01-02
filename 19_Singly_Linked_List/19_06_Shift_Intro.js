dd3796024cb0f97b17e2f9c900240fdcca03e8fb
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-02 18:07:20 +0530

    Add files via upload

---
+++ 19_Singly_Linked_List/19_06_Shift_Intro.js
@@ -0,0 +1,22 @@
+// Shifting:
+// Remoing a new node from the begyinning of the linked list.
+
+
+// Shifting Psudocode: 
+
+// * If there are no nodes, return undefined 
+
+// * Store the current head property in a variable 
+
+// * Set the head property to be current head's next property 
+
+// * Decrement the length by 1 
+
+// * Return the value of the node removed 
+
+
+
+
+
+
+
