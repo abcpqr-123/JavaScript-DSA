0771a04cb26796db337327af0b864877ba915dae
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-02 18:08:14 +0530

    Add files via upload

---
+++ 19_Singly_Linked_List/19_08_Unshift_Intro.js
@@ -0,0 +1,19 @@
+// Unshifting: 
+// Adding a new node to the beggining of the Linked List 
+
+
+// * This function should eccept the value 
+
+// * Create a new node using the vlaue passed to the function 
+
+// * If there is no head property on the list, Set the head and tail to 
+//   the newly created node 
+
+// * Otherwise set the newly created nodes next property to be the 
+//   current head property on the list 
+
+// * Set the head property on the list to be that newly created node 
+
+// * Increment the length of the list by 1 
+
+// * Return the linked list 
