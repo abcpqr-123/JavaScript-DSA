555fada1fd2feb8bf6ebd169bb2c8239126f9b96
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-02 18:09:40 +0530

    Add files via upload

---
+++ 19_Singly_Linked_List/19_12_Set_Intro.js
@@ -0,0 +1,17 @@
+// Set:
+// changing the vlaue of a node based on its position in the linked list 
+
+
+
+// Set Pseudocode 
+
+// * This function should accept a Value and an index 
+
+// * Use ur get function to find the specific node 
+
+// * If the node is not found, return false 
+
+// * If the node is found, set the value of that node to be the value 
+//   passed to the function and return true
+
+
