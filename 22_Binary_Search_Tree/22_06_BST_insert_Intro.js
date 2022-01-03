2e13faa8ba0b500ede2d76cbf55809f8f49f3f41
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-03 23:13:25 +0530

    Add files via upload

---
+++ 22_Binary_Search_Tree/22_06_BST_insert_Intro.js
@@ -0,0 +1,22 @@
+// Inserting a node: 
+
+// Steps - Iteratively Or Recusively 
+
+// * Create a new node 
+
+// * Starting at the root 
+
+//   * Check if there is a root, If not-the root now becomes that new Node! 
+//   * If there is a root, check if the value of the new node is greater than or 
+//     less than the value of the root 
+
+//     * If it is greater 
+//       # Check to see if there is a node to the right 
+//       # If there is not, add that node as the right property 
+
+//     * If it is less 
+//       # If there is, move to that node and repeat these steps 
+//       # If there is not, add that node as the rigtht property
+
+
+
