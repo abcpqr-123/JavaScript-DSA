6cfc6c7fefa224a94efa466e26fce423c45eabdb
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-03 23:14:03 +0530

    Add files via upload

---
+++ 22_Binary_Search_Tree/22_08_BST_Find_or_Search.js
@@ -0,0 +1,28 @@
+// Finding a node in a BST 
+
+// steps - Iteratively or Recursively 
+
+// * Starting at the root 
+
+//   # Check if threre is a root, If not-we're done Searching! 
+
+//   # If threre is a root, check if the value of the new node is 
+//     the value we are looking for, 
+//     If we found, we are done! 
+
+//   # If not, check to see if hte value is greater than the value 
+//     of the root 
+//   # if it is greater 
+
+//     * check to see if there is a node to the right 
+    
+//         # If there is, move to theat node and repeat these steps 
+//         # If there is not, We're done with searching 
+
+//    # If it is less 
+
+//      * check to see if there is a node to the left 
+   
+//         # If there is, move to that node and repeat these steps 
+//         # If there is not, we're done with searching!
+        
