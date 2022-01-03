288dc99283ba2931902ba3f8fe0cc908c9655339
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-03 23:11:25 +0530

    Add files via upload

---
+++ 22_Binary_Search_Tree/22_01_Intro_to_Trees.js
@@ -0,0 +1,43 @@
+// What is a tree? 
+
+// A data structure that consists of nodes in a parent/child relationship 
+
+
+// Note: Lists are - LINEAR
+
+//       Trees are - NONLINEAR 
+
+
+
+
+
+// tree Terminology:
+
+// Root - The top node in a tree 
+
+// Child - A node directly connected to another node when moving away from 
+//         the root 
+
+// parent - The converse notion of a child 
+
+// Siblings - A group of nodes with the same parent 
+
+// Leaf -  A node with no children 
+
+// Edge - The connection between one node and another 
+
+
+
+// ----------Trees Applications----------------------
+
+// * HTML DOM 
+
+// * Network Routing 
+
+// * Abstract Syntax Tree 
+
+// * Aratificial Intelligence 
+
+// * Folders in Operating Systems 
+
+// * Computer File System 
