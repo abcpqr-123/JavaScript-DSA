d686abf26f789ca7e3c5428a09def5266d215430
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-03 23:14:35 +0530

    Add files via upload

---
+++ 22_Binary_Search_Tree/22_10_Big_O_BST_Time_complexity.js
@@ -0,0 +1,26 @@
+// Bitg O of BST 
+
+// insertion - O(log n)- Best case, and everage 
+
+// Searching - O(log n)- Best case, and everage 
+
+// 2x number of nodes: 1 extra steps
+
+// 4x number of nodes: 2 extra steps
+
+// 8x number of nodes: 3 extra steps
+
+
+
+
+// but in case of WORST CASE iTS  - not guaranteed!
+    
+    3 --> 17 --> 19 --> 32 --> 34 --> 63 --> 86 --> 91
+
+// ex: if we take a binary tree, every node to the right is greater then its 
+//     parent 
+//     then the time complexity will be O(n)
+
+
+
+
