0964f7af154b1446473c6c94df7ecdc97d67ded2
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-03 23:12:12 +0530

    Add files via upload

---
+++ 22_Binary_Search_Tree/22_02_02_Intro_B_Search_Tree.js
@@ -0,0 +1,11 @@
+// Binary Search tree: 
+//    * Every parent node has at most two children 
+
+//    * Every node to the LEFT of a parent node is ALWAYS LESS 
+//      than the parent 
+
+//    * Every node to the RIGTH of a parent node is always GREATER than 
+//      the parent 
+
+
+
