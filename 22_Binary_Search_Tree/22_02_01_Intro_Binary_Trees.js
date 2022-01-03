172f3952fe7c3638777a4a58a205074af246021e
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-03 23:11:45 +0530

    Add files via upload

---
+++ 22_Binary_Search_Tree/22_02_01_Intro_Binary_Trees.js
@@ -0,0 +1,16 @@
+// tree: 
+// A data structure that consists of nodes in a parent/child relationship 
+
+
+// Binary Tree:
+// 
+
+
+// Binary Search tree: 
+//    * Every parent node has at most two children 
+
+//    * Every node to the LEFT of a parent node is ALWAYS LESS 
+//      than the parent 
+
+//    * Every node to the RIGTH of a parent node is always GREATER than 
+//      the parent 
