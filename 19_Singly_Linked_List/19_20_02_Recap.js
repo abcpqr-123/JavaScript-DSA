9970a4e6e87024e968ccc0809f4cd9eec8ffee36
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-03 23:06:34 +0530

    Add files via upload

---
+++ 19_Singly_Linked_List/19_20_02_Recap.js
@@ -0,0 +1,10 @@
+// RECAP 
+
+// Singly Linked Lists are an excellent alternative to arrays 
+// when insertion and deletion at the begginning are frequently 
+// required 
+
+// Arrays contain a built in index whereas Linked Lists do not 
+
+// The idea of list data structure that consists of nodes is the 
+// foundation for other data structures like stacks and Queues 
