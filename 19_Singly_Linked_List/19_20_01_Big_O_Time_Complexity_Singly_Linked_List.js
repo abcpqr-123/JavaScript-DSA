e06f016a8d316469d3af9bc4a42955eef458b2e5
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-03 23:06:18 +0530

    Add files via upload

---
+++ 19_Singly_Linked_List/19_20_01_Big_O_Time_Complexity_Singly_Linked_List.js
@@ -0,0 +1,15 @@
+// Big O of singly Linked List 
+
+// Insertion - O(1)
+
+// Removal - It Depends --> O(1) OR O(N)
+//    if u r removing form the begging its very easy  O(1)
+//    If u r removing from the end its little difficult O(N)
+        //   Beacuse we need to find the item right before the tail 
+        //   and it to do that we need to iterate  the entire list 
+                //  best case - O(1)
+                //  worst case - O(n)
+
+// Searching - O(N)
+
+// Access - O(N)
