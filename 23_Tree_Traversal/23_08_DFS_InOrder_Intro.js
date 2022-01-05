97ab63d90c3e16659667ec8ba4a6592d6ef99bb8
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-05 07:35:23 +0530

    Add files via upload

---
+++ 23_Tree_Traversal/23_08_DFS_InOrder_Intro.js
@@ -0,0 +1,33 @@
+// DFS - InOrder
+
+
+//                 10 
+    
+//            6           15 
+
+//         3     8             20 
+
+
+[3,6,8,10,15,20]
+
+
+// DFS - InOrder 
+
+// steps - Recusively 
+
+// * Create a variable to store the values of nodes visited 
+
+// * Store the root of the BST in a variable called current 
+
+// * Write a helper function which accepts a node 
+  
+//     # If the node has a left property, call the helper function 
+//       with the left property on the node 
+    
+//     # Push the value of the node to the variable that stores the 
+//       values 
+
+//     # If the node has a rigth property, call the helper function 
+//       wuth the rigth property on the node 
+
+// * Invoke the helper function with the current variable 
