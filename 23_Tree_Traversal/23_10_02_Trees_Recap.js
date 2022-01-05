0399a7737aa5df07948c6a4da0078bd546929418
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-05 07:36:56 +0530

    Add files via upload

---
+++ 23_Tree_Traversal/23_10_02_Trees_Recap.js
@@ -0,0 +1,21 @@
+// Recap: 
+
+// * Trees are non-linear data structure that contain a root and 
+//   child nodes 
+
+// * Binary Trees can have values of any type, but at most two children 
+//   for each parent 
+
+// * Binary Search Trees are a more specific version of Binary Search 
+//   Trees Where every node to the left of a parent is less than its 
+//   value and every node to the right is greater 
+
+// * We Can Search through Trees using BFS and DFS 
+
+// * In-DFS there are mainly three types: 
+  
+//     # InOrder 
+
+//     # PreOrder 
+     
+//     # PostOrder 
