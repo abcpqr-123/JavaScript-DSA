66b34df8cae8525b440778f36c5e5fffd207bbab
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-05 07:34:31 +0530

    Add files via upload

---
+++ 23_Tree_Traversal/23_06_DFS_PostOrder_intro.js
@@ -0,0 +1,33 @@
+//           ---->      10 
+    
+//        ---->    6           15 
+
+//     ---->    3     8             20 
+
+[3,8,6,20,15,10]
+
+
+// DFS - PostOrder 
+
+// Steps - Recursively 
+
+// * Create a variable to store the values of nodes visited 
+
+// * Store the root of the BST  in a variable called current 
+
+// * Write a helper function which accepts a node 
+  
+//   # If the node has a left property, call the helper function 
+//     with the left property on the node 
+
+//   # If the node has a right property, call the helper function 
+//     with the right property on the node 
+
+//   # Push the value of the node to the variable that stores the
+//     values 
+
+
+//   # Invoke the helper function with the current variable 
+
+
+
