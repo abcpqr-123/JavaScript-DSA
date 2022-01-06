3a8911a32c5e3c1c1577ef91dd6d4959eeb59f5d
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-06 13:14:23 +0530

    Add files via upload

---
+++ 26_Graphs/26_07_Add_Vertex_Intro.js
@@ -0,0 +1,22 @@
+// Graph class 
+
+// class Graph {
+//     constructor(){
+//         this.adjacencyList = {}
+//     }
+// }
+
+
+// Adding a vertex: 
+
+// write a method called addVertex, which accepts a name of a vertex 
+
+// It should add a key to the adjacency list with the name of the 
+// vertex and set its value to be an empty array 
+
+
+// g.addVertex ("Tokyo")
+
+// {
+//     "Tokyo" : []
+// }
