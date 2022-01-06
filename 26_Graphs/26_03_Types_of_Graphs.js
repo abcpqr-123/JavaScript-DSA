83aef176153f5e1c129223ccf80fdb30ce6e44f1
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-06 13:13:22 +0530

    Add files via upload

---
+++ 26_Graphs/26_03_Types_of_Graphs.js
@@ -0,0 +1,59 @@
+// Essential Graph Terms: 
+
+// * Vertex - a node 
+
+// * Edge - connection between nodes 
+
+// * Weigthted/Unweigthed - values assigned to distances between vertices 
+
+// * Directed/Undirected - Directions assigned to distanced between vertices 
+
+
+// undirected Graph: 
+
+//  The graph in which there is no direction to the connections is known as 
+//  Undirected Graph.  It represents 2-way connection
+//     If A and B are connections then B and A are also connetions 
+//     There will we be direction associated with edge 
+
+// Ex: Facebook Friends Graph 
+
+//     Surya can see the content of Andrew and Andrew can see the content 
+//     Of Surya 
+
+
+
+// Directed Graph: 
+
+// The graph in which there is a direction assigned to an edge and edge counld 
+// be bidirectional or unidirectional i.e It could point both way, but it also 
+// could point one way. 
+
+// There is a direction assingned to the edge. 
+
+// Ex: Social notwork model of instagram 
+//     Ex: Surya follows Sachin, but Sachin wont follows Surya 
+
+
+
+// Weighted Graph: 
+// When we assign the values to the edges of the graph it becomes weighted 
+// graph. 
+
+// Ex: * Google maps are the example for weighted undirected graph 
+
+//     * Instagram can be the example for both directed and undirected graphs 
+    //    For example: Instagram tracks of who follows and who watches the content 
+    //                 of a celebrity the most 
+
+
+
+// UnWeighted Graph: 
+
+// Ex:    * Instagram followers Graph is a UnWeighted directed Graph
+        
+//        * Facebook Friends graph is UnWeighted Undirected Graph 
+
+
+ 
+
