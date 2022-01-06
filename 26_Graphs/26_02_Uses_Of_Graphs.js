1734f30cd250662c8996500e1cd23d6da40e55c4
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-06 13:12:54 +0530

    Add files via upload

---
+++ 26_Graphs/26_02_Uses_Of_Graphs.js
@@ -0,0 +1,27 @@
+// Uses for Graphs: 
+
+// * Social Networks: 
+//   Ex:Meta 
+
+
+// * Location / Mapping 
+//   Ex: Google Maps
+
+
+
+// * Recommendations Like:
+
+//     # Pleople aslo watched 
+   
+//     # You might also like 
+
+//     # People u might know 
+
+//     # Frequently bought with 
+
+
+// * Visual Hierarchy 
+
+// * File system Optimizations 
+
+// * Routing
