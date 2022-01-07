25faaacdb95ca4e0b3a44b5f109f38c34940f904
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-07 05:58:30 +0530

    Add files via upload

---
+++ 27_Graph_Traversal/27_08_BFS_Intro.js
@@ -0,0 +1,23 @@
+// BREADTH FIRST 
+
+// * This function should accept a starting vertex 
+
+// * Create a queue (u can use an array) and place the sarting vertex in it 
+  
+// * Create an array to store the nodes cisited 
+
+// * Create an object to store nodes visited 
+
+// * Mark the starting vertex as visited 
+
+// * Loop as long s there is anything in the queue 
+
+// * Remove the first vertex from the queue and push it into the array that 
+//   stores nodes visited 
+
+// * Loop over each vertex in the adjacency list for the vertex u r visiting 
+
+// * If it is not inside the obhect that stores nodes visited, mark it as visited 
+//   and enqueue that vertex 
+
+// * Once u have finished looping, return the array of visited nodes 
