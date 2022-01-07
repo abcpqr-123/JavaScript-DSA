e2782a30010b50bb7ea4ca61ae7dc0aac30d39fd
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-07 05:54:44 +0530

    Add files via upload

---
+++ 27_Graph_Traversal/27_01_Graph_Traversal.js
@@ -0,0 +1,25 @@
+// Graph Traversal:
+    //    Visiting/Updating/Checking/ Each vertex in a graph 
+
+
+// Graph Traversal Uses:
+   
+//    * Peer to peer Networking
+//    * Web carwlers 
+//    * Finding 'closest'
+//      matches/recommendations 
+            // Ex: # Netflix movie recommendations
+            //     # Facebook Frieds Graph: 
+            //       * Recommending friends: 
+            //         who ever has most friends of friends with the person 
+            //         recommed all those people
+                  
+             
+//    * Shortest path problems 
+//         #  GPS Navigation 
+//         #  Solving mazes 
+//         #  AI (Shortest Path to win the game)
+
+
+
+    
