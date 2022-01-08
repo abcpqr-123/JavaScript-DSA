374aee5ab2499efddb3108874673f97ed7f6c092
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-08 18:07:55 +0530

    Add files via upload

---
+++ 24_Binary_Heaps/24_03_Heap_Insert_Intro.js
@@ -0,0 +1,37 @@
+// Defining Our Class:
+
+// Class Name: 
+//   MaxBinaryHeap 
+// Properties: 
+// values = []
+
+
+// Insert Pseudocode: 
+
+// * Push value into the values property on the heap 
+
+// * Bubble the value up to its correct spot! 
+
+
+///----------------------------------------------------------
+
+// // Insert Pseudocode in detail: 
+
+// * Push the value into the vlaues propety on the heap 
+
+// * Bubble Up: 
+//    # Create a variable called index which is the length of the values 
+//      proprty - 1
+
+//    # Create a variable called parentIndex whcich is the floor of 
+//      (index - 1)/ 2
+
+//    # Keep looping as ling as the values e4lement at the parentIndex is
+//      less than the values element at the child index 
+//          * Swap the value of the values element at the parentindex with 
+//            the value of the element property at the child index 
+//          * Set the index to be the parentIndex, and start over!
+       
+
+
+
