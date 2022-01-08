c8d0f8b7cb75eef92078db2190e46604ad3752b0
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-08 18:11:19 +0530

    Add files via upload

---
+++ 24_Binary_Heaps/24_07_Priority_Queue_Intro.js
@@ -0,0 +1,14 @@
+// What is a Priority queue? 
+
+// A data structure where each Element with higher priorities are 
+// served before elements with lower proprities
+
+
+// A naive version: 
+// Use a list to store all elements 
+
+// Priority: 3   Prority: 1   Priority: 2   Prority: 5   Prority: 4
+
+// iterate over the entire thing to find the highest priority element 
+
+
