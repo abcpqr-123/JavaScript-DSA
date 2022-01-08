3fe1c3f7d5829f359004078257d8e22cd5f57ce6
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-08 18:11:44 +0530

    Add files via upload

---
+++ 24_Binary_Heaps/24_08_Priority_Queue_Pseudocode.js
@@ -0,0 +1,45 @@
+// Creating class for heap is almost same but we have to add some 
+// extra stufs 
+
+// Class Name: 
+//    PriorityQueue
+// Proprities: 
+//    values=[]
+
+
+
+
+// But also we have to add: 
+// Class Name: 
+//     Node 
+// Properties: 
+//    val 
+//    priority 
+
+
+// val doesnot matter.
+// Heap is constructed using proirity 
+
+// val: "pay bill"
+// Priority: 1 
+
+// val: 'walk dog'
+// Priority: 2 
+
+// val: 'go Out'
+// Priority: 3
+
+
+
+// PseudoCode: 
+
+// Write a min binary Heap - lower number means higher priority.
+
+// Each Node has a val and a priority. Use the priority to buid 
+// the heap 
+
+// Enqueue method accepts a value and priority, makes a new node, 
+// and puts it in the right spot based off of its priority 
+
+// Dequeue method removes root element, returns it, and rearranges 
+// heap using priority.
