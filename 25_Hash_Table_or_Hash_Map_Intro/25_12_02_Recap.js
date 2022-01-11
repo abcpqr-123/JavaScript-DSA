ee99e13b1bf4caf9fd9efa9c655c49164bfde9ad
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-11 13:52:23 +0530

    Add files via upload

---
+++ 25_Hash_Table_or_Hash_Map_Intro/25_12_02_Recap.js
@@ -0,0 +1,18 @@
+// Recap: 
+
+// * Hash tables are collections of key-Value pairs 
+
+// * Hash tables can find values quickly given a key 
+
+// * Hash tables can add new key-values quickly 
+
+// * Hash tables store data in a large arrayy and work y hashing the keys 
+
+// * A goog hash slould be fast, distribute keys uniformly, and 
+//   be deterministic 
+
+// * Separate chiaing and linear probing are twqo strategies used to deal 
+//   with two keys that hash to the same index 
+
+
+
