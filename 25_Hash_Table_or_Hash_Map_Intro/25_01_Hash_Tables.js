22dc826d46f1060434b1c811fec4f493367a1a67
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-11 13:46:56 +0530

    Add files via upload

---
+++ 25_Hash_Table_or_Hash_Map_Intro/25_01_Hash_Tables.js
@@ -0,0 +1,31 @@
+// What is a Hash Table? 
+
+// * Hash Tables are used to store key-value pairs 
+
+// * They are like arrays, but the keys are not ordered 
+
+// * Unlike arrays, hash Tables are fast for all of the  following 
+//   operations: Finding values, adding new values , and removing 
+//   values!
+
+
+
+// Why One should care? What is the importece of Hash tables? 
+
+// * Nearly every programming language has some sort of hash table data 
+//   data structure 
+
+// * Beacuse of their speed, hash tables are very commanly used!
+
+
+// Hash Tables in the wild: 
+
+// Python has Dicionaries 
+
+// JS has Objects and Maps*
+
+// Java, Go and Scala have Maps 
+
+// Ruby has Hashes 
+
+// Note: Objects have some restrictions, but are basically hash tahbles
