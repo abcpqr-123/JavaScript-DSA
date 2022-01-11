3f81cd1c9ebc59a3a68831cf9e9894d78c961dd9
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-11 13:51:58 +0530

    Add files via upload

---
+++ 25_Hash_Table_or_Hash_Map_Intro/25_12_01_Big_O_Time_Complexity.js
@@ -0,0 +1,14 @@
+// Big O of Hash Tables: 
+
+// ---> average case: 
+
+// * Insert O(1)
+
+// * Deletion: O(1)
+
+// * Access: O(1)
+
+
+// If the hash function is storing everything at one index then to 
+// delete or insert from the end then it will take
+// worst case -  O(n)
