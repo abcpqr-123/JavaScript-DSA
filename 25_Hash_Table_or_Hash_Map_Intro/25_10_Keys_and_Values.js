0457e425f801f218ce905bf33efbc15a1c6bd465
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-11 13:50:19 +0530

    Add files via upload

---
+++ 25_Hash_Table_or_Hash_Map_Intro/25_10_Keys_and_Values.js
@@ -0,0 +1,11 @@
+// Key/Values
+
+// keys 
+
+// 1.Loop through the hash table array and returns an array of keys 
+//   in the table 
+
+// Values 
+
+// 1. Loops through the hash tables array and returns an array of values 
+//    in the table 
