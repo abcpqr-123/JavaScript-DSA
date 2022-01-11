b647f07e51925d2887ecdd03e2b576b4bb065b46
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-11 13:47:29 +0530

    Add files via upload

---
+++ 25_Hash_Table_or_Hash_Map_Intro/25_02_More_About_Hash_Tables.js
@@ -0,0 +1,27 @@
+// Hash Tables
+// Introductory example: 
+
+// How can we get human-readability and computer readability?
+// Computers dont know how to find an element at index pink! 
+
+
+// Thankfully we have hash tables to rescue! 
+
+
+//----------------------------------------
+
+// The Hash Part:
+
+// To implement a hast table, we'll be using an array.
+
+// In order to look up values by key, we need a way to convert 
+// keys into valid array indices.
+
+// A function that performs this task is called a hash function 
+
+
+
+// 0     1      2      3       4      5      6      7      8      9 
+// ^                   ^                            ^ 
+// ['Pink',          ['cyan',                      ['oranngered',
+// '#ff69b4']         '#00ffff']                     'ff4500']
